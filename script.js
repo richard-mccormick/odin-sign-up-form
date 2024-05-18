41f8a8e4a7a035568ce97654cef3be7dd6ee4795
Richard McCormick <mccormickri38@gmail.com>
2024-05-17 23:14:53 -0400

    Add phone number formatting

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log("IT'S WORKING!!!");
+
+function phoneNumberFormat(input) {
+    input = input.replace(/\D/g, "");
+    let size = input.length;
+
+    if (size > 0){
+        input = "("+input;
+    }
+    if (size > 3){
+        input = input.slice(0,4) + ")" + input.slice(4,11);
+    }
+    if (size > 6){
+        input = input.slice(0,8) + "-" + input.slice(8);
+    }
+    return input;
+}
